0fde7654609ad87b3386ac5887212c226d8867a1
nguyen minh dang <nguyenminhdangdev@gmail.com>
2025-06-18 16:41:58 +0700

    update change page

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse,name="index_id")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/trangchu", response_class=HTMLResponse,name="trangchu_id")
+async def read_root(request: Request):
+    return templates.TemplateResponse("trangchu.html", {"request": request})
+@app.get("/chatbox", response_class=HTMLResponse,name="chatbot_id")
+async def read_root(request: Request):
+    return templates.TemplateResponse("chatbox.html", {"request": request})
+@app.get("/audio", response_class=HTMLResponse,name="audio_id")
+async def read_root(request: Request):
+    return templates.TemplateResponse("audio.html", {"request": request})
 
 app.include_router(router)
 
